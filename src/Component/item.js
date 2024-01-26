b09bec5955c24fd32a397468d966fa6ca6f2485a
Alice DI NUNNO <alice@dinunno.fr>
2024-01-26 18:11:20 -0500

    [ui] updated borders

--- src/Component/item.js
+++ src/Component/item.js
@@ -7,7 +7,18 @@ export const Item = ({ children }) => {
         width: "12.5%",
         height: "100%",
         backgroundColor: "black",
-        border: "1px solid #34495e",
+
+        borderLeft: "1px solid #7f8c8d",
+        borderRight: "1px solid #7f8c8d",
+
+
+        borderTop: "2px solid #7f8c8d",
+        borderBottom: "2px solid #7f8c8d",
+
+        marginLeft: "-1px",
+        marginRight: "1px",
+        marginTop: "-1px",
+        marginBottom: "1px",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
